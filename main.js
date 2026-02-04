8626824878dc6c3d18b48fce6ae6c71aa34242a6
Nona Pareulidze <nona.pareulidze1@gmail.com>
2026-02-04 20:27:09 +0400

    fix: some bugs

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 let burger = document.querySelector(".burger");
 let list = document.querySelector(".header_menu ul");
 let listitems = document.querySelectorAll(".header_menu ul li");
-burger.addEventListener("click", togelforlist);
-burger.addEventListener("click", function(){
 
-});
 
-function togelforlist() {
+function toggleBurgerMenu() {
   document.body.classList.toggle("no_scroll");
   list.classList.toggle("burgerul");
   burger.classList.toggle("burgerstyle");
   burger.classList.toggle("active");
-  
 }
-burger.addEventListener("click", togelforlist);
-listitems.forEach((item) => {
-  item.addEventListener("click", togelforlist);
+burger.addEventListener("click", function () {
+  toggleBurgerMenu();
+  if (list.classList.contains("burgerul")) {
+    listitems.forEach((item) => {
+      item.addEventListener("click", toggleBurgerMenu);
+    });
+  } 
 });
